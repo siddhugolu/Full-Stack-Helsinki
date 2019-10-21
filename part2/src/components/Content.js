8231b3fe65ea5b6f93f97b64d6fdeda181dbfbdc
Siddhartha Kumar Golu <siddhugolu@gmail.com>
2019-10-21 11:57:04 +0530

    Added Exercise 2.3

--- part2/src/components/Content.js
+++ part2/src/components/Content.js
@@ -6,9 +6,12 @@ const Content = ({parts}) => {
 	const show = () => parts.map(part => 
 		<Part key={part.id} name={part.name} exercises={part.exercises} />
 	)
+
+	const total = () => parts.reduce((sum, part) => sum + part.exercises, 0)
 	return (
 		<div>
 		  {show()}
+		  Total of {total()} exercises
 		</div>
 	)
 }
