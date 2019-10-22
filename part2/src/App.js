f0271edbc969b89d392ded345671ad1317a936f0
Siddhartha Kumar Golu <siddhugolu@gmail.com>
2019-10-22 11:36:26 +0530

    Added Exercise 2.5

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,35 +2,60 @@ import React from 'react';
 import Course from './components/Course';
 
 const App = () => {
-	const course = {
-		name: 'Half Stack application development',
-		parts: [
-		  {
-		  	name: 'Fundamentals of React',
-		  	exercises: 10,
-		  	id: 1
-		  },
-		  {
-		  	name: 'Using props to pass data',
-		  	exercises: 7,
-		  	id: 2
-		  },
-		  {
-		  	name: 'State of a component',
-		  	exercises: 14,
-		  	id: 3
-		  },
-		  {
-		  	name: 'Redux',
-		  	exercises: 11,
-		  	id: 4
-		  }
-		]
-	}
+	const course = [
+		{
+			name: 'Half Stack application development',
+			id: 1,
+			parts: [
+			  {
+			  	name: 'Fundamentals of React',
+			  	exercises: 10,
+			  	id: 1
+			  },
+			  {
+			  	name: 'Using props to pass data',
+			  	exercises: 7,
+			  	id: 2
+			  },
+			  {
+			  	name: 'State of a component',
+			  	exercises: 14,
+			  	id: 3
+			  },
+			  {
+			  	name: 'Redux',
+			  	exercises: 11,
+			  	id: 4
+			  }
+			]
+		},
+		{
+			name: 'Node.js',
+			id: 2,
+			parts: [
+				{
+					name: 'Routing',
+					exercises: 3,
+					id: 1
+				},
+				{
+					name: 'Middlewares',
+					exercises: 7,
+					id: 2
+				}
+			]
+
+		}
+	]
+
+	const showCourses = () => course.map(c => 
+		<Course key={c.id} course={c} />
+	)
 
 	return (
 		<div>
-		  <Course course={course} />
+		  <h1> Web Development Curriculum </h1>
+		  {showCourses()}
 		</div>
 	)
 }
--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -1,8 +1,40 @@
 import React from 'react';
-import Header from './Header';
-import Content from './Content';
+
+const Header = ({name}) => {
+	return (
+		<div>
+		  <h2> {name} </h2>
+		</div>
+	)
+}
+
+const Part = ({name, exercises}) => {
+	return (
+		<div>
+		  <p> {name} {exercises} </p>
+		</div>
+	)
+}
+
+
+const Content = ({parts}) => {
+
+	const show = () => parts.map(part => 
+		<Part key={part.id} name={part.name} exercises={part.exercises} />
+	)
+
+	const total = () => parts.reduce((sum, part) => sum + part.exercises, 0)
+
+	return (
+		<div>
+		  {show()}
+		  <h3> Total of {total()} exercises </h3>
+		</div>
+	)
+}
 
 const Course = ({course}) => {
+
 	return (
 		<div>
 		  <Header name={course.name} />
