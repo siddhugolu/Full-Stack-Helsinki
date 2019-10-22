bf81b216dc2a80e9601b5787e14256e7e9eaeedd
Siddhartha Kumar Golu <siddhugolu@gmail.com>
2019-10-22 15:39:43 +0530

    Working with forms and event handlers

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,61 +1,51 @@
-import React from 'react';
-import Course from './components/Course';
+import React, { useState } from 'react';
+import Note from './components/Note';
 
-const App = () => {
-	const course = [
-		{
-			name: 'Half Stack application development',
-			id: 1,
-			parts: [
-			  {
-			  	name: 'Fundamentals of React',
-			  	exercises: 10,
-			  	id: 1
-			  },
-			  {
-			  	name: 'Using props to pass data',
-			  	exercises: 7,
-			  	id: 2
-			  },
-			  {
-			  	name: 'State of a component',
-			  	exercises: 14,
-			  	id: 3
-			  },
-			  {
-			  	name: 'Redux',
-			  	exercises: 11,
-			  	id: 4
-			  }
-			]
-		},
-		{
-			name: 'Node.js',
-			id: 2,
-			parts: [
-				{
-					name: 'Routing',
-					exercises: 3,
-					id: 1
-				},
-				{
-					name: 'Middlewares',
-					exercises: 7,
-					id: 2
-				}
-			]
+const App = (props) => {
+	const [notes, setNotes] = useState(props.notes)
+	const [newNote, setNewNote] = useState('')
+	const [showAll, setShowAll] = useState(true)
 
-		}
-	]
+	const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
 
-	const showCourses = () => course.map(c => 
-		<Course key={c.id} course={c} />
+	const rows = () => notesToShow.map(note => 
+		<Note key={note.id} note={note} />
 	)
+
+	const addNote = (event) => {
+		event.preventDefault()
+		const noteObject = {
+			id: notes.length + 1,
+			content: newNote,
+			date: new Date().toISOString(),
+			important: Math.random() > 0.5
+		}
+
+		setNotes(notes.concat(noteObject))
+		setNewNote('')
+	}
+
+	const handleNoteChange = (event) => {
+		console.log(event.target.value)
+		setNewNote(event.target.value)
+	}
+
 
 	return (
 		<div>
-		  <h1> Web Development Curriculum </h1>
-		  {showCourses()}
+		  <h1> Notes </h1>
+		  <div>
+			  <button onClick={() => setShowAll(!showAll)}>
+			    show {showAll ? 'important' : 'all'}
+			  </button>
+		  </div>
+		  <ul>
+		    {rows()}
+		  </ul>
+		  <form onSubmit={addNote}>
+		    <input value={newNote} onChange={handleNoteChange}/>
+		    <button type='submit'> Save </button>
+		  </form>
 		</div>
 	)
 }
