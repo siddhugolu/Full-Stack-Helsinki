4924e5cb97c62d9e7783824b3c3e4e3243f07830
siddhugolu <siddhugolu@gmail.com>
2019-10-24 22:02:06 +0530

    Modifications for displaying countries via Fake-REST API

--- part2/src/App.js
+++ part2/src/App.js
@@ -7,28 +7,50 @@ const App = () => {
 	// const [newNote, setNewNote] = useState('')
 	// const [showAll, setShowAll] = useState(true)
 
-	const [persons, setPersons] = useState([])
+	// const [persons, setPersons] = useState([])
 
-	useEffect(() => {
-		console.log('effect')
-		axios.get('http://localhost:3001/persons')
-			.then(response => {
-				console.log('promise fulfilled')
-				setPersons(response.data)
-			})
-	}, [])
-	console.log('render', persons.length, 'persons')
+	const [countries, setCountries] = useState([])
 
-	const showPhone = () => persons.map(p => 
-		<div key={p.id}>
-			{p.name} {p.number}
+	// const renderLanguage = () => countries.languages.map(c => 
+	// 	<div>
+	// 		<ul>
+	// 			<li key={c.name}>
+	// 				{c.name}
+	// 			</li>
+	// 		</ul>
+	// 	</div>
+
+	// )
+
+	const showCountries = () => countries.map(c => 
+		<div key={c.numericCode}>
+			<h1> {c.name} </h1>
+			<div> Capital: {c.capital} </div>
+			<div> Population: {c.population} </div>
+			<h2> Languages </h2>
+			<p>
+				{c.languages.name}
+			</p>
+			
 		</div>
 	)
 
+	useEffect(() => {
+		console.log('effect')
+		axios.get('https://restcountries.eu/rest/v2/all')
+			.then(response => {
+				console.log('promise fulfilled')
+				setCountries(response.data)
+			})
+	}, [])
+	console.log('render', countries.length, 'countries')
+	console.log({countries})
+
+
 	return (
 		<div>
-			<h1> Working with Phonebook </h1>
-			{showPhone()}
+			<h1> Working with Countries </h1>
+			{showCountries()}
 		</div>
 	)
 }
