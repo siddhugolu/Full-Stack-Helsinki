767565dbbce0b50cc03a38f53400ae98adbc3175
Siddhartha Kumar Golu <siddhugolu@gmail.com>
2019-11-03 23:47:49 +0530

    Add exercise 3.8 - phonebook backend step 8

--- part3/index.js
+++ part3/index.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
