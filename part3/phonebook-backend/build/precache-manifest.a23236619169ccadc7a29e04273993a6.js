a7a7aea0202c5aac15dcaa27cae7d069bbf1ee52
Siddhartha Kumar Golu <siddhugolu@gmail.com>
2019-11-08 15:26:10 +0530

    Add exercise 3.14: phonebook database, step 2

---
+++ part3/phonebook-backend/build/precache-manifest.a23236619169ccadc7a29e04273993a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434e5c10d8dcb80b73c05ad6ea3a061c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e1fb2ac699f65702cf0",
+    "url": "/static/css/main.f929fba0.chunk.css"
+  },
+  {
+    "revision": "771a5316c89f382b4bd3",
+    "url": "/static/js/2.09f0f0c9.chunk.js"
+  },
+  {
+    "revision": "8e1fb2ac699f65702cf0",
+    "url": "/static/js/main.a27f0589.chunk.js"
+  },
+  {
+    "revision": "17d0ae5722a7a1d90858",
+    "url": "/static/js/runtime-main.86de38e2.js"
+  }
+]);
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const Phonebook = require('./models/phonebook')
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -12,88 +14,35 @@ app.use(express.static('build'))
 morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        name: "Arto hellas",
-        number: "39-44-5323523",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "889920-1839",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "99201-129-23",
-        id: 3
-    },
-    {
-        name: "Siddhartha",
-        number: "882991-289",
-        id: 4
-    }
-]
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
-})
-
-app.get('/info', (req, res) => {
-
-    const date = new Date()
-    res.send(`
-    <p> Phonebook has info for ${persons.length} people </p>
-    <p> Request received at: ${date} </p>
-    `)
+    Phonebook.find({}).then(p => {
+        res.json(p.map(r => r.toJSON()))
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-
-    if(person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Phonebook.findById(req.params.id).then(p => {
+        res.json(p.toJSON())
+    })
 })
-
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    res.status(204).end()
-})
-
-const generateId = () => {
-    return Math.floor(Math.random() * Math.floor(10000))
-}
 
 app.post('/api/persons', (req, res) => {
     const body = req.body
-    const pos = persons.findIndex(p => p.name === body.name)
-    if(!body.name || !body.number) {
-        return res.status(400).json({
-            error: 'Name/Number is missing'
-        })
-    }
-    
-    if(pos !== -1) {
-        return res.status(400).json({
-            error: 'Name must be unique'
-        })
+
+    if(body.name === undefined || body.number === undefined) {
+        return res.status(400).json({ error: 'Name/number missing' })
     }
 
-    const person = {
+    const entry = new Phonebook({
         name: body.name,
         number: body.number,
-        id: generateId()
-    }
+    })
 
-    persons = persons.concat(person)
-    res.json(person)
+    entry.save().then(savedEntry => {
+        res.json(savedEntry.toJSON())
+    })
 })
+
 
 const unknownEndpoint = (req, res) => {
     res.status(404).send({
@@ -103,7 +52,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const port = process.env.PORT || 3001
-app.listen(port, () => {
-    console.log(`Server running on ${port}`)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
 })
