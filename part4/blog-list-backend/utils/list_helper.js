d43f860c0889ddd9a56c687fa51b6181527abe18
Siddhartha Kumar Golu <siddhartha.kumar.golu@intel.com>
2019-11-14 16:00:56 +0530

    Add exercise 4.7: helper functions and unit tests, step5

--- part4/blog-list-backend/utils/list_helper.js
+++ part4/blog-list-backend/utils/list_helper.js
@@ -1,12 +1,93 @@
-const dummy = (blogs) => {
-    return 1
-}
-
 const totalLikes = (blogs) => {
     return blogs.map(b => b.likes).reduce((acc, item) => acc+item, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    let maxLikes = {
+        title: "",
+        author: "",
+        likes: null
+    }
+    if(!blogs.length) return null
+    blogs.forEach(item => {
+        if(item.likes > maxLikes.likes) {
+            maxLikes = item
+        }
+    })
+    return {
+        title: maxLikes.title,
+        author: maxLikes.author,
+        likes: maxLikes.likes
+    }
+}
+
+const mostBlogs = (blogs) => {
+    const blogsMap = new Map()
+    blogs.forEach(item => {
+        if(blogsMap.has(item.author)) {
+            let getBlogs = blogsMap.get(item.author)
+            getBlogs++
+            blogsMap.set(item.author, getBlogs)
+        } else {
+            blogsMap.set(item.author, 1)
+        }
+    })
+
+    let result = {
+        author: "",
+        blogs: null
+    }
+    if(!blogs.length) return null
+    let count = 0
+    blogsMap.forEach((value, key) => {
+
+        if(value > count) {
+            count = value
+            result = {
+                author: key,
+                blogs: value
+            }
+        }
+    })
+
+    return result
+}
+
+const mostLikes = (blogs) => {
+    const blogsMap = new Map()
+    blogs.forEach(item => {
+        if(!blogsMap.has(item.author)) {
+            blogsMap.set(item.author, item.likes)
+        } else {
+            let getLikes = blogsMap.get(item.author)
+            getLikes += item.likes
+            blogsMap.set(item.author, getLikes)
+        }
+    })
+
+    let result = {
+        author: "",
+        likes: null
+    }
+
+    if(!blogs.length) return null
+    let count = 0
+    blogsMap.forEach((value, key) => {
+        if(value > count) {
+            count = value
+            result = {
+                author: key,
+                likes: value
+            }
+        }
+    })
+
+    return result
+}
+
 module.exports = {
-    dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
