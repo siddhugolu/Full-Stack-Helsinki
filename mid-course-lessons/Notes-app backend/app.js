1108a538478e3621a32a689b8c9110fe49ed3572
Siddhartha Kumar Golu <siddhartha.kumar.golu@intel.com>
2019-11-21 00:22:58 +0530

    Add till mid-course 'testing with backend async/await' - part 4

---
+++ mid-course-lessons/Notes-app backend/app.js
@@ -0,0 +1,33 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('Connecting to...', config.MONGODB_URI)
+
+mongoose.set('useFindAndModify', false)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        logger.info('Connected to MongoDB!')
+    })
+    .catch(error => {
+        logger.info('error while connecting to MongoDB: ', error.message)
+    })
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static('build'))
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
