f66a6af9d7d845f9a341dd5e4331a4cd9edc707e
Siddhartha <sid@thelazyoxymoron.me>
2020-01-17 12:23:59 +0530

    Added exercise 4.18: bloglist expansion, step 7

--- part4/blog-list-backend/app.js
+++ part4/blog-list-backend/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
@@ -27,6 +30,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
