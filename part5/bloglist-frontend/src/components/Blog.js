377e2fbb027d91107e2ca9b9294e9ad0f55d0710
Siddhartha <sid@thelazyoxymoron.me>
2020-03-03 17:40:20 +0530

    Added Exercise 5.6: Blog list frontend, step 6 - display the whole information about blog post when the title is clicked

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} ------> {blog.author}
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    console.log('clicked')
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+  <div style={blogStyle}>
+    <div onClick={toggleVisibility} style={hideWhenVisible}>
+      {blog.title} ------> {blog.author}
+    </div>
+    <div onClick={toggleVisibility} style={showWhenVisible}>
+      <div> {blog.title} </div>
+      <div> {blog.author} </div>
+      <div> {blog.url} </div>
+      <div> {blog.likes} likes <button> like </button> </div>
+      <div> Added by {blog.user.name} </div>
+    </div>
   </div>
-)
+)}
 
 export default Blog
