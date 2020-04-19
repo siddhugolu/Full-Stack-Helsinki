9a21f66493fffcfb4011127198f67379d97033de
Siddhartha <sid@thelazyoxymoron.me>
2020-04-19 17:22:36 +0530

    Added mid course lessons till part5/props_children_and_prototypes end

--- mid-course-lessons/Notes-app-frontend/src/components/LoginForm.js
+++ mid-course-lessons/Notes-app-frontend/src/components/LoginForm.js
@@ -1,36 +1,45 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({
-    username,
-    password,
-    handleUsernameChange,
-    handlePasswordChange,
-    handleSubmit
+  username,
+  password,
+  handleUsernameChange,
+  handlePasswordChange,
+  handleSubmit
 }) => {
-    return (
-        <div>
-            <h2> Login </h2>
+  return (
+    <div>
+      <h2> Login </h2>
 
-            <form onSubmit={handleSubmit}>
-                <div>
+      <form onSubmit={handleSubmit}>
+        <div>
                     username
-                    <input
-                      value={username}
-                      onChange={handleUsernameChange}
-                    />
-                </div>
-                <div>
-                    password
-                    <input
-                      type="password"
-                      value={password}
-                      onChange={handlePasswordChange}
-                    />
-                </div>
-                <button type="submit"> login </button>
-            </form>
+          <input
+            value={username}
+            onChange={handleUsernameChange}
+          />
         </div>
-    )
+        <div>
+                    password
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit"> login </button>
+      </form>
+    </div>
+  )
+}
+
+LoginForm.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
 }
 
 export default LoginForm
